8bdf74aeff0ef8dc78d29ffa45cf603f1b487da6
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-23 17:14:46 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ Employee_Payroll_ADO.NET/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Employee_Payroll_ADO.NET
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee_Payroll Database with ADO.NET");
+        }
+    }
+}
